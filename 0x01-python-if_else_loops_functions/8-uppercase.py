a44307d5bfb0024edab8118d50e2bf71d19a9009
basmalaalii <basmalaalii175@gmail.com>
2023-11-01 17:13:37 -0700

    a

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
 
-uppercase("best")
-uppercase("Best School 98 Battery street")
+def uppercase(str):
+    for c in str:
+        print("{:c}"
+                .format(ord(c) if not islower(c) else ord(c) - 32),
+                end="")
+        print("")
