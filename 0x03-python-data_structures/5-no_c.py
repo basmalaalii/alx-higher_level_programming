b646d3dde37302bfd88236f280291b8c1a5e972e
basmalaalii <basmalaalii175@gmail.com>
2023-11-06 16:04:52 -0800

    b

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
 def no_c(my_string):
-    ret = ""
-    for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'C'):
-            ret += my_string[i]
-            return ret
+  ret = ""  # Initialize the ret variable to an empty string.
+  for i in range(len(my_string)):
+    if (my_string[i] != 'c' and my_string[i] != 'C'):
+      ret += my_string[i]
+  return ret
