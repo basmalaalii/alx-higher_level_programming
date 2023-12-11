c0a4e07945b0dacae9443e0917784f319a24d497
basmalaalii <basmalaalii175@gmail.com>
2023-12-11 10:52:43 -0800

    d

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-'''Module for square class.'''
+'''Module for Square class.'''
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    '''ASquare class.'''
+    '''A Square class.'''
 
     def __init__(self, size, x=0, y=0, id=None):
-        '''Construtor.'''
+        '''Constructor.'''
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
@@ -24,3 +24,26 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        '''Internal method that updates instance attributes via */**args.'''
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''Updates instance attributes via no-keyword & keyword args.'''
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+    def to_dictionary(self):
+        '''Returns dictionary representation of this class.'''
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
